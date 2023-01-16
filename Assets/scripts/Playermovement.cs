96150b232633cefda65189fccf7c3ff91f047aa8
TheSnowly <thesnowly18@gmail.com>
2023-01-16 12:10:52 +0100

    fast falling

--- Assets/scripts/Playermovement.cs
+++ Assets/scripts/Playermovement.cs
@@ -7,11 +7,13 @@ public class Playermovement : MonoBehaviour
     Rigidbody2D rb;
     SpriteRenderer sr;
     float horizontal_value;
+    float vertical_value;
     Vector2 ref_velocity = Vector2.zero;
 
-    float jumpForce = 5f;
+    float jumpForce = 10f;
 
     [SerializeField] float moveSpeed_horizontal = 700.0f;
+    [SerializeField] float moveSpeed_vertical = 700.0f;
     [SerializeField] bool is_jumping = false;
     [SerializeField] bool can_jump = false;
     [Range(0, 1)][SerializeField] float smooth_time = 0.5f;
@@ -29,6 +31,7 @@ public class Playermovement : MonoBehaviour
     void Update()
     {
         horizontal_value = Input.GetAxis("Horizontal");
+        vertical_value = Input.GetAxis("Vertical");
 
         if(horizontal_value > 0) sr.flipX = false;
         else if (horizontal_value < 0) sr.flipX = true;
@@ -48,6 +51,11 @@ public class Playermovement : MonoBehaviour
         }
         Vector2 target_velocity = new Vector2(horizontal_value * moveSpeed_horizontal * Time.fixedDeltaTime, rb.velocity.y);
         rb.velocity = Vector2.SmoothDamp(rb.velocity, target_velocity, ref ref_velocity, 0.3f);
+
+        if (vertical_value < 0) {
+            Vector2 target_velocity_falling = new Vector2(rb.velocity.x , vertical_value * moveSpeed_vertical * Time.fixedDeltaTime);
+            rb.velocity = Vector2.SmoothDamp(rb.velocity, target_velocity_falling, ref ref_velocity, 0.5f);
+        }
         
     }
     private void OnTriggerStay2D(Collider2D collision)
